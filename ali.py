5ccfa38aa3f02a5a67fd0c1d3cadf8d11db8b790
exitinfo <wngus_569@naver.com>
2018-12-09 00:42:44 -0800

    ali

--- ali.py
+++ ali.py
@@ -13,10 +13,12 @@ item_price = "<span class=[\"]value[\"] itemprop=[\"]price[\"]>.*?</span>"
 #item_name = "<a class=[\"]history-tiem prodect [\"] href=.*? title=.*? target=.*?>"
 item_name = "<span itemprop=[\"]name[\"].*?</span>"
 item_img = "<img[^>].*? src=[\"].*?jpg"
+item_link = "<a id=[\"]limagebox.*? href=[\"].*?[\"]"
 #print (re.search(item_price, res.text))
 #print (re.search(item_name, res.text))
 tag = "<[^>].*?>"
 im_tag = "<img[^>].*? src=[\"]"
+li_tag = "<a id=[\"]limagebox.*? href=[\"]"
 price = re.findall(item_price, res.text)
 #print(price)
 for i in price:
@@ -32,7 +34,13 @@ for j in name:
 
 
 img = re.findall(item_img, res.text)
-print (img)
+#print (img)
 for k in img:
 	im = re.sub(im_tag, "", k)
 	print(im)
+
+link = re.findall(item_link, res.text)
+#print(link)
+for l in link:
+	li = re.sub(li_tag, "", l)
+	print(li)

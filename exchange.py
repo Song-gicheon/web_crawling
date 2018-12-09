31072395b4967b0aa091d2173382d11d60544652
exitinfo <wngus_569@naver.com>
2018-12-10 04:13:37 +0900

    Exchange Rate

--- exchange.py
+++ exchange.py
@@ -9,18 +9,20 @@ res = requests.get(url)
 res.status_code
 res.text
 #print (res.text)
-item_price = "<span class=[\"]s-item__price[\"]>.*?</span>"
-item_name = "<h3 class=[\"]s-item__title[\"] role=[\"]text[\"].*?</h3>"
-item_img = "<img[^>].*? src=[\"].*?jpg"
+#item_price = "<span class=[\"]s-item__price[\"]>.*?</span>"
+#item_name = "<h3 class=[\"]s-item__title[\"] role=[\"]text[\"].*?</h3>"
+#item_img = "<img[^>].*? src=[\"].*?jpg"
 exchange = "<em>USD</em></span>.*?</span>"
 #print (re.search(item_price, res.text))
 #print (re.search(item_name, res.text))
 tag = "<[^>].*?>"
-im_tag = "<img[^>].*? src=[\"]"
-
+#im_tag = "<img[^>].*? src=[\"]"
+usd_tag = "USD "
 
 exc = re.findall(exchange, res.text)
+#print(re.sub(tag, "", exc[0]))
+print (re.sub(usd_tag, "", re.sub(tag, "", exc[0])))
 #print (exc)
-for i in exc:
-	im = re.sub(tag, "", i)
-	print(im)
+#for i in exc:
+#	im = re.sub(tag, "", i)	
+#	print(im)

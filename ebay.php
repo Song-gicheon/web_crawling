8316c5541b81a4146079a05b0ee3bbf459959af3
exitinfo <wngus_569@naver.com>
2018-12-10 20:35:44 +0900

    eBay

--- ebay.php
+++ ebay.php
@@ -4,31 +4,38 @@
 	$page = $_GET['page'];
 	$item = str_replace(" ", "+", $item);
 	$handler = shell_exec("./ebay.py $item $page");
+	$regex_price = '/[0-9\.]+/';
+	
+	$conn = mysqli_connect("localhost", "root", "hsd", "web");
+	$sql = "select * from exchange";
+	$result = mysqli_query($conn, $sql);
+	$exchange = mysqli_fetch_array($result);
 
 	
 	// 여기서 각 정보 구분해서 출력
 	// 전체 handler 에서 종류별로 구분.
-
 	$product = 0;
 	$arr_data = array();
 	$arr_line = explode("\n", $handler);
 	foreach($arr_line as $line)
 	{
 		// 이미지 링크
-		if(strpos($line, "ebayimg") > 0)
+		if(strpos($line, "jpg") > 0)
 		{
 			$product++;
 			$arr_data[$product]['img'] = $line;
 		}
 		// 이동 경로
-		elseif(strpos($line, "ebay.com") > 0)
+		elseif(strpos($line, "itm") > 0)
 		{
 			$arr_data[$product]['path'] = $line;
 		}
 		// 가격
 		elseif(strpos($line, "strong")>0)
 		{
-			$arr_data[$product]['price'] = $line;
+			preg_match($regex_price, $line, $pri);
+			$pr = $pri[0] * $exchange['rate'];
+			$arr_data[$product]['price'] =(int)$pr."<br>"."$ ".$line;	
 		}
 		//제품명
 		elseif(strpos($line, "span") > 0)
@@ -36,7 +43,7 @@
 			$arr_data[$product]['prod'] = $line;
 		}
 	}
-	for ($row = 1; $row<$product; $row++){
+	for ($row = 1; $row<=$product; $row++){
 		echo '<a href="'.$arr_data[$row]['path'].'" target="_blank"><li class="prod_item"><div class="item_block"><div class="item_img"><img src="'.$arr_data[$row]['img'].'" width="100%"></img></div><div class="item_title">'.$arr_data[$row]['prod'].'</div><div class="item_price">₩'.$arr_data[$row]['price'].'</div></div></li></a>';
 	}
 
--- ebay.py
+++ ebay.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+# -*- coding: utf-8 -*-
 import requests
 from html.parser import HTMLParser
 import re
@@ -8,7 +8,8 @@ import sys
 item = sys.argv[1]
 page = sys.argv[2]
 
-url = "https://www.ebay.com/sch/i.html?_from=R40&_nkw="+item+"&_pgn"+page
+#url = "https://www.ebay.com/sch/i.html?_from=R40&_nkw="+item+"&_pgn"+page
+url = "https://www.ebay.com/sch/i.html?_from=R40&_nkw="+item+"&_sacat=0&LH_TitleDesc=0&LH_TitleDesc=0&_ipg=25&_pgn="+page
 res = requests.get(url)
 res.status_code
 res.text
@@ -25,22 +26,29 @@ im_tag = "<img[^>].*? src=[\"]"
 im2_tag = "https://ir.*? data-src=[\"]"
 li_tag = "<div class=[\"]s-item__image[\"]><a href=[\"]"
 li2_tag = "[\"] tabindex"
-
+#na_tag = "[\"\'\”\-\=\.\#\/\?\!\:\$\}\ó]"
+na_tag = "[^a-z^A-z^0-9^\s\`]"
 img = re.findall(item_img, res.text)
 price = re.findall(item_price, res.text)
 name = re.findall(item_name, res.text)
 link = re.findall(item_link, res.text)
-
+print(price)
 size = len(img)
 for i in range(size):
+	print('test1')
 	if (img[i].find("gif") > 0):
 		print(re.sub(im2_tag, "", re.sub(im_tag, "", img[i])))	
 	else:
 		print(re.sub(im_tag, "", img[i]))
 	print(re.sub(li2_tag, "", re.sub(li_tag, "", link[i])))
-	print("<strong>"+re.sub(tag, "", price[i])+"</strong>")
-	print("<span>"+re.sub(tag, "", name[i])+"</span>")
-
+	if (price[i].find("to") > 0):
+		print("<strong>"+re.sub("\$", "",re.sub("to", "", re.sub(tag, "", price[i])))+"</strong>")
+	else:
+		print("<strong>"+re.sub("\$", "", re.sub(tag, "", price[i]))+"</strong>")
+	na = re.sub(tag, "", name[i])
+	print("<span>"+re.sub(na_tag, "", na)+"</span>")
+	print('test')
+print('test2')
 #print (img)
 #for k in img:
 #	im = re.sub(im_tag, "", k)
